130fcd7480f88b945a58e7aa823f9235d4178284
LeonardoSá <32676285+leodesa@users.noreply.github.com>
2018-04-14 11:09:56 -0300

    A

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+ var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
